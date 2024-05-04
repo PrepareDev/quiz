5ee037a7636c4c31ce13f6c615bb5d11a909d091
Nikita Tyrkin <tyrkinn@pm.me>
2024-05-04 20:54:13 +0300

    fix(env): compute auth url

--- src/env.js
+++ src/env.js
@@ -51,7 +51,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL: getAuthUrl(),
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
