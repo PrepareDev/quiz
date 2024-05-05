fd0e3fda46e58617fb692e3abc77b065e31e1ca6
snusEbjoer <mrkill2222@gmail.com>
2024-05-05 16:09:08 +0300

    fix: error handling

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -1,45 +1,47 @@
 import { z } from "zod";
-
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { categories } from "~/server/db/schema";
 
-export const categoryRouter = createTRPCRouter({ // test later
+export const categoryRouter = createTRPCRouter({
+  // test later
   create: protectedProcedure
-  .input(z.object({
-    name: z.string(),
-}))
-.output(z.object({
-  id: z.number(),
-  name: z.string(),
-}))
-  .mutation(async ({input, ctx}) => {
-    const category = await ctx.db
-    .insert(categories)
-    .values({
-        name: input.name,
-    }).returning()
-    if (category[0]?.id === undefined) {
-      throw new Error("internal server error")
-    }
-    return category[0]
-  }),
+    .input(
+      z.object({
+        name: z.string(),
+      }),
+    )
+    .output(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const category = await ctx.db
+        .insert(categories)
+        .values({
+          name: input.name,
+        })
+        .returning();
+      if (category.length < 1) {
+        throw new Error("Internal server error");
+      }
+      return category[0]!; // handled on line ahead
+    }),
   getAll: protectedProcedure
-  .output(z.array(
-    z.object({
-      id: z.number(),
-      name: z.string(),
-    })
-  ))
-  .query(async ({ctx}) => {
-    const category = await ctx.db
-    .select()
-    .from(categories)
-    if (category[0] === undefined) {
-      throw new Error("internal server error")
-    }
-    return category
-  })
+    .output(
+      z.array(
+        z.object({
+          id: z.number(),
+          name: z.string(),
+        }),
+      ),
+    )
+    .query(async ({ ctx }) => {
+      const category = await ctx.db.select().from(categories);
+      if (category === undefined) {
+        throw new Error("internal server error");
+      }
+      return category;
+    }),
 });
--- src/server/api/routers/quiz.ts
+++ src/server/api/routers/quiz.ts
@@ -1,27 +1,26 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { quizes } from "~/server/db/schema";
 
-export const quizRouter = createTRPCRouter({ // test later
+export const quizRouter = createTRPCRouter({
+  // test later
   create: protectedProcedure
-  .input(z.object({
-    name: z.string(),
-    category_id: z.number(),
-}))
-  .mutation(async ({ctx, input}) => {
-    const user = ctx.session.user
-    const quiz = await ctx.db
-    .insert(quizes)
-    .values({
-        name: input.name, 
+    .input(
+      z.object({
+        name: z.string(),
+        category_id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = ctx.session.user;
+      const quiz = await ctx.db
+      .insert(quizes)
+      .values({
+        name: input.name,
         creator_id: user.id,
         category_id: input.category_id,
-    })
-    console.log(quiz)
-    return quiz
-  })
+      });
+      return quiz;
+    }),
 });
